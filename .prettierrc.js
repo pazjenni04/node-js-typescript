5633f7ee2e7a6296b6c65d3ff3fb88bf4a457b86
Jennifer Davila <pazjenni1331@gmail.com>
2021-12-19 14:33:58 -0600

    set up structure of files and installed all necessary packages.  Working on getting mongo connection

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+// file just sets up the style in which you want your code to display in or format that you want it
+// to follow
+
+module.exports = {
+  tabWidth: 4,
+  singleQuote: true,
+  semi: true,
+};
